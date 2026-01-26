3e3557766f834018775f2aa4a16552309046e081
Itswali <luckywali0986@gmail.com>
2026-01-26 14:58:15 +0500

    created models for each data group all the models together create a program to initialize the default model value use auth to call and set the default value for models for new users

---
+++ lib/models/index.ts
@@ -0,0 +1,7 @@
+import "./board";
+import "./column";
+import "./job-applicattion";
+
+export { default as Board } from "./board";
+export { default as Column } from "./column";
+export { default as JobApplication } from "./job-applicattion";
